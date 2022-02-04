bbf4017dc5a46b6484ad0d7e93a2557accc71490
Juan Carlos <jcarretero@elpuig.xeill.net>
2022-02-04 22:17:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/puigram/Post.java
@@ -0,0 +1,27 @@
+package com.example.puigram;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Post {
+    public String uid;
+    public String author;
+    public String authorPhotoUrl;
+    public String content;
+    public String mediaUrl;
+    public String mediaType;
+    public Map<String, Boolean> likes = new HashMap<>();
+    public long date;
+
+    // Constructor vacio requerido por Firestore
+    public Post() {}
+    public Post(String uid, String author, String authorPhotoUrl, String content, String mediaUrl, String mediaType, long date) {
+        this.uid = uid;
+        this.author = author;
+        this.authorPhotoUrl = authorPhotoUrl;
+        this.content = content;
+        this.mediaUrl = mediaUrl;
+        this.mediaType = mediaType;
+        this.date = date;
+    }
+}
